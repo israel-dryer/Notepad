8dfad2e8ac509cae00e02b7e47b3d197c7b03e9c
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-10-07 15:29:39 -0400

    added type hints

--- notepad.py
+++ notepad.py
@@ -9,35 +9,38 @@
 import PySimpleGUI as sg 
 sg.ChangeLookAndFeel('BrownBlue') # change style
 
-WIN_W = 90
-WIN_H = 25
-STARTUP = True
-
-filename = None
+WIN_W: int = 90
+WIN_H: int = 25
+STARTUP: bool = True
+filename:str = None
 
 # string varables to shorten loop and menu code
-file_new = 'New............(CTRL+N)'
-file_open = 'Open..........(CTRL+O)'
-file_save = 'Save............(CTRL+S)'
+file_new: str = 'New............(CTRL+N)'
+file_open: str = 'Open..........(CTRL+O)'
+file_save: str = 'Save............(CTRL+S)'
 
-menu_layout = [['File',[file_new, file_open, file_save,'Save As','---','Exit']],
-               ['Tools',['Word Count']],
-               ['Help',['About']]]
-layout = [[sg.Menu(menu_layout)],
-          [sg.Text('> New file <', font=('Consolas',10), size=(90,1), key='_INFO_')],
-          [sg.Multiline(font=('Consolas',12), size=(WIN_W, WIN_H), key='_BODY_')]]
-window = sg.Window('Notepad', layout=layout, margins=(0,0), resizable=True, return_keyboard_events=True)
+menu_layout: list = [['File',[file_new, file_open, file_save,'Save As','---','Exit']],
+                     ['Tools',['Word Count']],
+                     ['Help',['About']]]
+layout: list = [[sg.Menu(menu_layout)],
+                [sg.Text('> New file <', font=('Consolas',10), size=(90,1), key='_INFO_')],
+                [sg.Multiline(font=('Consolas',12), size=(WIN_W, WIN_H), key='_BODY_')]]
 
-def new_file():
+window: object = sg.Window('Notepad', layout=layout, margins=(0,0), resizable=True, return_keyboard_events=True)
+
+
+def new_file() -> str:
+    ''' Reset body and info bar, and clear filename variable '''
     window['_BODY_'].update(value=None)
     window['_INFO_'].update(value='> New File <')
     filename = None
     return filename
 
-def open_file():
-    ''' open file and update the infobar '''
+
+def open_file() -> str:
+    ''' Open file and update the infobar '''
     try:
-        filename = sg.popup_get_file('Open File', no_window=True)
+        filename: str = sg.popup_get_file('Open File', no_window=True)
     except:
         return
     if filename not in (None, '') and not isinstance(filename, tuple):
@@ -46,8 +49,9 @@ def open_file():
         window['_INFO_'].update(value=filename)
     return filename
 
-def save_file(filename):
-    ''' save file instantly if already open; otherwise use `save-as` popup '''
+
+def save_file(filename: str):
+    ''' Save file instantly if already open; otherwise use `save-as` popup '''
     if filename not in (None, ''):
         with open(filename,'w') as f:
             f.write(values.get('_BODY_'))
@@ -55,10 +59,11 @@ def save_file(filename):
     else:
         save_file_as()
 
-def save_file_as():
-    ''' save new file or save existing file with another name '''
+
+def save_file_as() -> str:
+    ''' Save new file or save existing file with another name '''
     try:
-        filename = sg.popup_get_file('Save File', save_as=True, no_window=True)
+        filename: str = sg.popup_get_file('Save File', save_as=True, no_window=True)
     except:
         return
     if filename not in (None, '') and not isinstance(filename, tuple):
@@ -67,15 +72,19 @@ def save_file_as():
         window['_INFO_'].update(value=filename)
     return filename
 
+
 def word_count():
-    ''' display estimated word count '''
-    words = values['_BODY_'].split(' ')
-    words_clean = [w for w in words if w!='\n']
-    word_count = len(words_clean)
+    ''' Display estimated word count '''
+    words: list = values['_BODY_'].split(' ')
+    words_clean: list = [w for w in words if w!='\n']
+    word_count: int = len(words_clean)
     sg.PopupQuick('Word Count: {:,d}'.format(word_count), auto_close=False)
 
+
 def about_me():
+    ''' Display a pithy message '''
     sg.PopupQuick('"All great things have small beginnings" - Peter Senge', auto_close=False)
+
 
 while True:
     event, values = window.read(timeout=1)
