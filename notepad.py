93e056ba349d1e4dc6d911b72b28563e193c279d
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2020-06-20 15:16:05 -0400

    refactored code

--- notepad.py
+++ notepad.py
@@ -2,95 +2,83 @@
   A minimalist Notepad built with the PySimpleGUI TKinter framework
   Author:     Israel Dryer
   Email:      israel.dryer@gmail.com
-  Modified:   2019-10-13
+  Modified:   2020-06-20
 '''
-import PySimpleGUI as sg 
+import PySimpleGUI as sg
+import pathlib
 sg.ChangeLookAndFeel('BrownBlue') # change style
 
-WIN_W: int = 90
-WIN_H: int = 25
-filename:str = None
+WIN_W = 90
+WIN_H = 25
+file = None
 
-# string variables to shorten loop and menu code
-file_new: str = 'New............(CTRL+N)'
-file_open: str = 'Open..........(CTRL+O)'
-file_save: str = 'Save............(CTRL+S)'
+menu_layout = [['File', ['New (Ctrl+N)', 'Open (Ctrl+O)', 'Save (Ctrl+S)', 'Save As', '---', 'Exit']],
+              ['Tools', ['Word Count']],
+              ['Help', ['About']]]
 
-menu_layout: list = [['File', [file_new, file_open, file_save, 'Save As', '---', 'Exit']],
-                     ['Tools', ['Word Count']],
-                     ['Help', ['About']]]
+layout = [[sg.Menu(menu_layout)],
+          [sg.Text('> New file <', font=('Consolas', 10), size=(WIN_W, 1), key='_INFO_')],
+          [sg.Multiline(font=('Consolas', 12), size=(WIN_W, WIN_H), key='_BODY_')]]
 
-layout: list = [[sg.Menu(menu_layout)],
-                [sg.Text('> New file <', font=('Consolas', 10), size=(WIN_W, 1), key='_INFO_')],
-                [sg.Multiline(font=('Consolas', 12), size=(WIN_W, WIN_H), key='_BODY_')]]
-
-window: object = sg.Window('Notepad', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True)
-window.read(timeout=1)
+window = sg.Window('Notepad', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True, finalize=True)
 window.maximize()
 window['_BODY_'].expand(expand_x=True, expand_y=True)
 
-def new_file() -> str:
-    ''' Reset body and info bar, and clear filename variable '''
+def new_file():
+    '''Reset body and info bar, and clear filename variable'''
     window['_BODY_'].update(value='')
     window['_INFO_'].update(value='> New File <')
-    filename = None
-    return filename
+    file = None
+    return file
 
-def open_file() -> str:
-    ''' Open file and update the infobar '''
-    try:
-        filename: str = sg.popup_get_file('Open File', no_window=True)
-    except:
-        return
-    if filename not in (None, '') and not isinstance(filename, tuple):
-        with open(filename, 'r') as f:
-            window['_BODY_'].update(value=f.read())
-        window['_INFO_'].update(value=filename)
-    return filename
+def open_file():
+    '''Open file and update the infobar'''
+    filename = sg.popup_get_file('Open', no_window=True)
+    if filename:
+        file = pathlib.Path(filename)
+        window['_BODY_'].update(value=file.read_text())
+        window['_INFO_'].update(value=file.absolute())
+        return file
 
-def save_file(filename: str):
-    ''' Save file instantly if already open; otherwise use `save-as` popup '''
-    if filename not in (None, ''):
-        with open(filename,'w') as f:
-            f.write(values.get('_BODY_'))
-        window['_INFO_'].update(value=filename)
+def save_file(file):
+    '''Save file instantly if already open; otherwise use `save-as` popup'''
+    if file:
+        file.write_text(values.get('_BODY_'))
     else:
         save_file_as()
 
-def save_file_as() -> str:
-    ''' Save new file or save existing file with another name '''
-    try:
-        filename: str = sg.popup_get_file('Save File', save_as=True, no_window=True)
-    except:
-        return
-    if filename not in (None, '') and not isinstance(filename, tuple):
-        with open(filename,'w') as f:
-            f.write(values.get('_BODY_'))
-        window['_INFO_'].update(value=filename)
-    return filename
+def save_file_as():
+    '''Save new file or save existing file with another name'''
+    filename = sg.popup_get_file('Save As', save_as=True, no_window=True)
+    if filename:
+        file = pathlib.Path(filename)
+        file.write_text(values.get('_BODY_'))
+        window['_INFO_'].update(value=file.absolute())
+        return file
 
 def word_count():
-    ''' Display estimated word count '''
-    words: list = [w for w in values['_BODY_'].split(' ') if w!='\n']
-    word_count: int = len(words)
-    sg.PopupQuick('Word Count: {:,d}'.format(word_count), auto_close=False)
+    '''Display estimated word count'''
+    words = [w for w in values['_BODY_'].split(' ') if w!='\n']
+    word_count = len(words)
+    sg.popup_no_wait('Word Count: {:,d}'.format(word_count))
 
 def about_me():
-    sg.PopupQuick('"All great things have small beginnings" - Peter Senge', auto_close=False)
+    '''A short, pithy quote'''
+    sg.popup_no_wait('"All great things have small beginnings" - Peter Senge')
 
 while True:
     event, values = window.read()
-
-    if event in (None, 'Exit'):
+    print(event)
+    if event in('Exit', None):
         break
-    if event in (file_new, 'n:78'):
-        filename = new_file()
-    if event in (file_open, 'o:79'):
-        filename = open_file()
-    if event in (file_save, 's:83'):
-        save_file(filename)
+    if event in ('New (Ctrl+N)', 'n:78'):
+        file = new_file()
+    if event in ('Open (Ctrl+O)', 'o:79'):
+        file = open_file()
+    if event in ('Save (Ctrl+S)', 's:83'):
+        save_file(file)
     if event in ('Save As',):
-        filename = save_file_as()   
+        file = save_file_as()   
     if event in ('Word Count',):
         word_count() 
     if event in ('About',):
